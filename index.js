6ed1390b0b8e9227a027b297646a5d57344206fb
Andrew9806-lang <kornilovaamina20@gmail.com>
2025-10-16 21:01:27 +0300

    change

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const root = document.querySelector("#root");
+const button2 = document.querySelector("#background-button2");
 const button = document.querySelector("#background-button");
 
 const changeBackground = ()=>{
